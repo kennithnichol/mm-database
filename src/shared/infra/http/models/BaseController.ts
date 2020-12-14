c9a036a8495de629271d6d88b60c8d0c558ab4c3
Kennith Nichol <ken.nichol@live.ca>
2020-12-14 14:41:02 -0800

    remove unecessary debug statement

--- src/shared/infra/http/models/BaseController.ts
+++ src/shared/infra/http/models/BaseController.ts
@@ -64,7 +64,6 @@ export abstract class BaseController {
     }
 
     public fail (res: express.Response, error: Error | string) {
-        console.log(error);
         return res.status(500).json({
             mesage: error.toString()
         })
